77e0f6798f1d94c1580a569beb2e135337ba3b6e
theoddkene <anazofelix@gmail.com>
2023-07-13 20:21:03 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using the printf function
+ * a piece of art, " followed by a new line.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\a");
+	return (0);
+}

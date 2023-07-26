819c4fd021fe34c9e80e0aeaf984ac7c4a814d5a
theoddkene <anazofelix@gmail.com>
2023-07-25 18:41:16 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,16 +6,18 @@
  * main - program that generates random valid
  * passwords for the program 101-crackme
  *
- *Return: Always 0 (success)
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-        int pass[100];
-        int i, sum, n;
-        sum = 0;
-        srand(time(NULL));
-	
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;
+
+	srand(time(NULL));
+
 	for (i = 0; i < 100; i++)
 	{
 		pass[i] = rand() % 78;
@@ -23,7 +25,7 @@ int main(void)
 		putchar(pass[i] + '0');
 		if ((2772 - sum) - '0' < 78)
 		{
-			n + 2772 - sum - '0';
+			n = 2772 - sum - '0';
 			sum += n;
 			putchar(n + '0');
 			break;

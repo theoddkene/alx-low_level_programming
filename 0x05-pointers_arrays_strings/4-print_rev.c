6dc95d481ff61cd4d2da332fc0ea64ce4600e7e9
theoddkene <anazofelix@gmail.com>
2023-07-25 17:33:39 -0700

    print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,18 @@
 void print_rev(char *s)
 {
 	int i;
-	int count = 0;
+	int longi = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-		count++;
-	for (i = count - !; i >= 0; i--)
-		_putchar(s[i]);
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
 	_putchar('\n');
 }

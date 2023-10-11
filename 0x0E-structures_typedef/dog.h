9db37dbd0a44207c8c29b5e5605f77d39438129b
theoddkene <anazofelix@gmail.com>
2023-10-10 19:13:49 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,6 +9,7 @@
  *
  * Description: Longer description
  */
+
 struct dog
 {
 	char *name;
@@ -26,6 +27,6 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
-int_strlen(char *s);
+int _strlen(char *s);
 
 #endif
